d3338ce671a3d457fdc64b34b21b8eba9ac2953c
Minsung Kim <minsungkim@Minsungui-MacBookPro.local>
2020-04-22 09:57:33 +0900

    Add user model

--- src/api/sayBye/sayBye.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-    Query: {
-        sayBye: () => "Bye everyone~"
-    }
-}
-
-export default resolvers;
--- src/api/sayHello/sayhello.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-    Query: {
-        sayHello: () => "hi everyone~"
-    }
-}
-
-export default resolvers;
